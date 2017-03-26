95f6b9ddc7494f7fbdce3f8ff4b2d07d5b1f9d92
w0lfschild <aguywithlonghair@yahoo.com>
2017-03-25 23:09:08 -0700

    1.1.2
    - Native blur
    - Hide weather copyright info box
    - Better implementation

--- darkNC/darkNC.m
+++ darkNC/darkNC.m
@@ -9,16 +9,18 @@
 @import AppKit;
 #import "ZKSwizzle.h"
 
+static const char * const newBackground = nil;
+
 @interface _WB_DNC_NotificationClear : NSObject
 @end
 
 @interface _WB_DNC_NCNotificationCenterWindowController : NSWindowController
 @end
 
-@interface _WB_DNC_NCWindow : NSPanel
+@interface _WB_DNC_NCThirdPartyDisclosuresViewController : NSViewController
 @end
 
-@interface _WB_DNC_NCThirdPartyDisclosuresViewController : NSViewController
+@interface CABackdropLayer : CALayer
 @end
 
 @implementation _WB_DNC_NotificationClear
@@ -27,7 +29,7 @@
     NSUInteger osx_ver = [[NSProcessInfo processInfo] operatingSystemVersion].minorVersion;
     if (osx_ver >= 12) {
         ZKSwizzle(_WB_DNC_NCNotificationCenterWindowController, NCNotificationCenterWindowController);
-        ZKSwizzle(_WB_DNC_NCWindow, _NCWindow);
+        ZKSwizzle(_WB_DNC_NCThirdPartyDisclosuresViewController, NCThirdPartyDisclosuresViewController);
     }
     NSLog(@"Notification Clear Loaded");
 }
@@ -61,27 +63,53 @@
     }
 }
 
+- (void)wb_changeBackgroundColor {
+    NSVisualEffectView *aview = ZKHookIvar(self, NSVisualEffectView*, "_texturedBackground");
+    CALayer *alayer = ZKHookIvar(aview, CALayer*, "_materialLayer");
+    CALayer *tlayer = ZKHookIvar(alayer, CALayer*, "_tintLayer");
+    CABackdropLayer *blayer = ZKHookIvar(alayer, CABackdropLayer*, "_backdropLayer");
+    
+    NSVisualEffectView *darkNCBackground = objc_getAssociatedObject(self, newBackground);
+    
+    if (darkNCBackground == nil) {
+        darkNCBackground = [[NSVisualEffectView alloc] initWithFrame:aview.frame];
+        [darkNCBackground setAppearance:[NSAppearance appearanceNamed:NSAppearanceNameVibrantDark]];
+        [aview addSubview:darkNCBackground positioned:NSWindowBelow relativeTo:nil];
+        objc_setAssociatedObject(self, newBackground, darkNCBackground, OBJC_ASSOCIATION_RETAIN);
+    }
+    
+    NSString *osxMode = [[NSUserDefaults standardUserDefaults] stringForKey:@"AppleInterfaceStyle"];
+    if ([osxMode isEqualToString:@"Dark"]) {
+        [tlayer setHidden:true];
+        [blayer setHidden:true];
+        [darkNCBackground setHidden:false];
+    } else {
+        [tlayer setHidden:false];
+        [blayer setHidden:false];
+        [darkNCBackground setHidden:true];
+    }
+}
+
 - (void)tabChanged:(id)arg1 {
+    [self wb_changeBackgroundColor];
     [self wb_changeTextColor:ZKHookIvar(self, NSView*, "_contentView")];
     ZKOrig(void, arg1);
 }
 
 - (void)willBeShown {
+    [self wb_changeBackgroundColor];
     [self wb_changeTextColor:ZKHookIvar(self, NSView*, "_contentView")];
     ZKOrig(void);
 }
 
 @end
 
-@implementation _WB_DNC_NCWindow
+@implementation _WB_DNC_NCThirdPartyDisclosuresViewController
 
-- (BOOL)inLiveResize {
-    NSString *osxMode = [[NSUserDefaults standardUserDefaults] stringForKey:@"AppleInterfaceStyle"];
-    if ([osxMode isEqualToString:@"Dark"])
-        self.backgroundColor = [NSColor colorWithSRGBRed:0.0 green: 0.0 blue: 0.0 alpha:0.65];
-    else
-        self.backgroundColor = [NSColor clearColor];
-    return ZKOrig(BOOL);
+- (void)_updateView {
+    NSTextView *view = ZKHookIvar(self, NSTextView*, "_textView");
+    [view setHidden:true];
+    // Nothing
 }
 
 @end
